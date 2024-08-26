718b3151580b5983d88225a44ed261f33cf01e93
Panida Jettana <panida.jett@hotmail.com>
2024-08-26 19:11:32 +0700

    updat

--- nodeConfig.js
+++ nodeConfig.js
@@ -1,33 +1,16 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
 'use strict';
+import { getDefaultWebpackConfig } from "@microsoft/vscode-azext-dev";
 
-//@ts-check
+import { DEBUG_WEBPACK } from './webpack.config';
 
-// See https://github.com/Microsoft/vscode-azuretools/wiki/webpack for guidance
-
-const process = require('process');
-const dev = require("@microsoft/vscode-azext-dev");
-const newLocal = newFunction();
-const webpack = newLocal;
-const path = require('path');
-const { nodeConfig } = require('./nodeConfig');
-
-let DEBUG_WEBPACK = !/^(false|0)?$/i.test(process.env.DEBUG_WEBPACK || '');
-const nodeSrcPath = path.resolve(__dirname, 'src/node');
-
-const alias = {};
-alias[nodeSrcPath] = path.resolve(__dirname, 'src/browser');
-
-let webConfig = dev.getDefaultWebpackConfig({
+let nodeConfig = getDefaultWebpackConfig({
     projectRoot: __dirname,
     verbosity: DEBUG_WEBPACK ? 'debug' : 'normal',
-    externals:
-    {
+    externals: {
         // Fix "Module not found" errors in ./node_modules/websocket/lib/{BufferUtil,Validation}.js
         // These files are not in node_modules and so will fail normally at runtime and instead use fallbacks.
         // Make them as external so webpack doesn't try to process them, and they'll simply fail at runtime as before.
@@ -36,23 +19,8 @@ let webConfig = dev.getDefaultWebpackConfig({
         '../build/Release/bufferutil': 'commonjs ../build/Release/bufferutil',
         '../build/default/bufferutil': 'commonjs ../build/default/bufferutil',
     },
-    target: 'webworker',
-    plugins: [new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-    })],
-    suppressCleanDistFolder: true,
-    resolveFallbackAliases: {
-        'constants': false
-    },
-    alias
+    target: 'node',
+    suppressCleanDistFolder: true
 });
-
-if (DEBUG_WEBPACK) {
-    console.log('Config:', nodeConfig);
-}
-
-module.exports = [nodeConfig, webConfig];
-
-function newFunction() {
-    return require('webpack');
-}
+const _nodeConfig = nodeConfig;
+export { _nodeConfig as nodeConfig };
